a2633aa6fc400f191ca602ddb095c64ca1897a9e
Brad Grantham <grantham@plunk.org>
2019-03-14 14:20:14 -0700

    remove file with wrong name

--- simple.glsl
+++
@@ -1,8 +0,0 @@
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
-
-    // Output to screen
-    fragColor = vec4(uv, 1.0, 1.0);
-}
