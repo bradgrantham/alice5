50b6f363f4ad028df4549ba7a90be4651871817d
Lawrence Kesteloot <lk@teamten.com>
2019-03-29 23:36:41 -0700

    Add comments for structs and fields.

--- shade.cpp
+++ shade.cpp
@@ -1324,37 +1324,67 @@ std::map<glslang::TBasicType, std::string> BasicTypeToString = {
     {glslang::EbtString, "String"},
 };
 
+// A variable in memory, either global or within a function's frame.
 struct Variable
 {
+    // Key into the "types" map.
     uint32_t type;
+
+    // One of the values of SpvStorageClass (input, output, uniform, function, etc.).
     uint32_t storageClass;
+
+    // XXX Not sure, or NO_INITIALIZER.
     uint32_t initializer;
+
+    // Offset into the memory for the specific storage class.
     size_t offset;
 };
 
 
+// Entry point for the shader.
 struct EntryPoint
 {
+    // What kind of shader. Must be SpvExecutionModelFragment.
     uint32_t executionModel;
+
+    // Name of the function to call. This is typically "main".
     std::string name;
+
+    // XXX Don't know.
     std::vector<uint32_t> interfaceIds;
+
+    // XXX Don't know what an execution mode is.
     std::map<uint32_t, std::vector<uint32_t>> executionModesToOperands;
 };
 
+// XXX A decoration tells us something about a type or variable, but I'm not sure what, maybe
+// how to access it from outside the shader and the offset of fields within a struct.
 struct Decoration
 {
+    // The ID of a type or variable.
     uint32_t decoration;
+
+    // XXX Specific operands for this decoration.
     std::vector<uint32_t> operands;
 };
 
+// Information about the source code for the shader.
 struct Source
 {
+    // What language the shader was written in.
     uint32_t language;
+
+    // XXX Version of the shading language.
     uint32_t version;
+
+    // XXX Dunno.
     uint32_t file;
+
+    // XXX Filename of the shader source?
     std::string source;
 };
 
+// XXX Unused.
 struct MemberName
 {
     uint32_t type;
@@ -1362,47 +1392,72 @@ struct MemberName
     std::string name;
 };
 
+// Represents a "void" type.
 struct TypeVoid
 {
+    // XXX Not sure why we need this. Not allowed to have an empty struct?
     int foo;
 };
 
+// Represents an "int" type.
 struct TypeInt
 {
+    // Number of bits in the int.
     uint32_t width;
+
+    // Whether signed.
     uint32_t signedness;
 };
 
+// Represents a "float" type.
 struct TypeFloat
 {
+    // Number of bits in the float.
     uint32_t width;
 };
 
+// Represents a vector type.
 struct TypeVector
 {
+    // Type of the element. This is a key in the "types" map.
     uint32_t type;
+
+    // Number of elements.
     uint32_t count;
 };
 
+// Represents a function type.
 struct TypeFunction
 {
+    // Type of the return value. This is a key in the "types" map.
     uint32_t returnType;
+
+    // Type of each parameter, in order from left to right. These are keys in the "types" map.
     std::vector<uint32_t> parameterTypes;
 };
 
+// Represents a struct type.
 struct TypeStruct
 {
+    // Type of each element, in order. These are keys in the "types" map.
     std::vector<uint32_t> memberTypes;
 };
 
+// Represents a pointer type.
 struct TypePointer
 {
+    // Type of the data being pointed to. This is a key in the "types" map.
     uint32_t type;
+
+    // Storage class of the data. One of the values of SpvStorageClass (input,
+    // output, uniform, function, etc.).
     uint32_t storageClass;
 };
 
+// Represents an image type.
 struct TypeImage
 {
+    // XXX document these.
     uint32_t sampledType;
     uint32_t dim;
     uint32_t depth;
@@ -1413,53 +1468,93 @@ struct TypeImage
     uint32_t accessQualifier;
 };
 
+// XXX Dunno.
 struct TypeSampledImage
 {
+    // Type of the image. This is a key in the "types" map. XXX yes?
     uint32_t imageType;
 };
 
+// Union of all known types.
 typedef std::variant<TypeVoid, TypeFloat, TypePointer, TypeFunction, TypeVector, TypeInt, TypeStruct, TypeImage, TypeSampledImage> Type;
 
+// A constant value hard-coded in the program or implicitly used by the compiler.
 struct Constant
 {
+    // Type of the constant. This is a key in the "types" map.
     uint32_t type;
+
+    // Value of the bits. Floats are presented bitwise.
     uint32_t value;
 };
 
+// A function parameter. This is on the receiving side, to set aside an SSA slot
+// for the value received from the caller.
 struct FunctionParameter
 {
+    // Type of the parameter. This is a key in the "types" map.
     uint32_t type;
+
+    // Name of the parameter. This is a key in the "names" array.
     uint32_t id;
 };
 
+// A "Load" instruction. This loads data from memory into an SSA register.
 struct InsnLoad {
+    // Type of data being loaded. This is a key in the "types" map.
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
+
+    // SSA register of pointer to dereference.
     uint32_t pointerId;
+
+    // Memory access semantics, or NO_MEMORY_ACCESS_SEMANTIC.
     uint32_t memoryAccess;
 };
 
+// A "Store" instruction. Stores data from an SSA register to memory.
 struct InsnStore {
+    // SSA register of pointer to dereference for writing.
     uint32_t pointerId;
+
+    // SSA register to copy from.
     uint32_t objectId;
+
+    // Memory access semantics, or NO_MEMORY_ACCESS_SEMANTIC.
     uint32_t memoryAccess;
 };
 
+// An "AccessChain" instruction. It dereferences a struct's field. XXX probably
+// arrays too, and maybe vectors and matrices.
 struct InsnAccessChain {
+    // The return type of this access. This is a key in the "types" map. This
+    // could be a pointer type even if the field is a vector.
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
+
+    // XXX Don't know.
     uint32_t basePointerId;
+
+    // List of indices. These are keys into the "registers" map.
     std::vector<uint32_t> indexes;
 };
 
 struct InsnCompositeConstruct {
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
     std::vector<uint32_t> constituentIds;
 };
 
 struct InsnCompositeExtract {
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
     uint32_t compositeId;
     std::vector<uint32_t> indexes;
@@ -1467,12 +1562,16 @@ struct InsnCompositeExtract {
 
 struct InsnConvertSToF {
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
     uint32_t sourceId;
 };
 
 struct InsnFDiv {
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
     uint32_t operand1Id; // dividend
     uint32_t operand2Id; // divisor
@@ -1480,6 +1579,8 @@ struct InsnFDiv {
 
 struct InsnFunctionCall {
     uint32_t type;
+
+    // SSA register to write result to.
     uint32_t resultId;
     uint32_t functionId;
     std::vector<uint32_t> argumentIds;
@@ -2450,6 +2551,7 @@ struct Interpreter
 #endif
     }
 
+    // Returns whether the program should continue.
     bool step()
     {
         std::cout << "address " << pc << "\n";
