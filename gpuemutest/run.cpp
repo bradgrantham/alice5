3fba05ff8863ad158c2685c8579cd10c5a67e275
Brad Grantham <grantham@plunk.org>
2019-04-22 11:52:55 -0700

    my hands are typing words

--- gpuemutest/run.cpp
+++ gpuemutest/run.cpp
@@ -26,32 +26,57 @@ struct Memory
     {}
     uint8_t read8(uint32_t addr)
     {
-        assert(addr < memorybytes.size());
+        printf("read 8 from %08X\n", addr);
+        if(!(addr < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
         return memorybytes[addr];
     }
     uint16_t read16(uint32_t addr)
     {
-        assert(addr + 1 < memorybytes.size());
+        printf("read 16 from %08X\n", addr);
+        if(!(addr + 1 < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
         return *reinterpret_cast<uint16_t*>(memorybytes.data() + addr);
     }
     uint32_t read32(uint32_t addr)
     {
-        assert(addr + 3 < memorybytes.size());
+        printf("read 32 from %08X\n", addr);
+        if(!(addr + 3 < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
+        return *reinterpret_cast<uint32_t*>(memorybytes.data() + addr);
+    }
+    uint32_t read32quiet(uint32_t addr)
+    {
+        if(!(addr + 3 < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
         return *reinterpret_cast<uint32_t*>(memorybytes.data() + addr);
     }
     void write8(uint32_t addr, uint32_t v)
     {
-        assert(addr < memorybytes.size());
+        printf("write 8 bits of %08X to %08X\n", v, addr);
+        if(!(addr < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
         memorybytes[addr] = static_cast<uint8_t>(v);
     }
     void write16(uint32_t addr, uint32_t v)
     {
-        assert(addr + 1 < memorybytes.size());
+        printf("write 16 bits of %08X to %08X\n", v, addr);
+        if(!(addr + 1 < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
         *reinterpret_cast<uint16_t*>(memorybytes.data() + addr) = static_cast<uint16_t>(v);
     }
     void write32(uint32_t addr, uint32_t v)
     {
-        assert(addr + 3 < memorybytes.size());
+        printf("write %08X to %08X\n", v, addr);
+        if(!(addr + 3 < memorybytes.size())) {
+            throw std::runtime_error("read outside memory at " + std::to_string(addr));
+        }
         *reinterpret_cast<uint32_t*>(memorybytes.data() + addr) = v;
     }
 };
@@ -71,6 +96,15 @@ std::vector<uint8_t> readFileContents(std::string shaderFileName)
     return text;
 }
 
+template <class T>
+void disassemble(uint32_t pc, T& memory)
+{
+    std::cout << "pc :";
+    std::cout << std::hex << std::setw(8) << std::setfill('0') << pc;
+    std::cout << " " << std::hex << std::setw(8) << std::setfill('0') << memory.read32quiet(pc);
+    std::cout << std::setw(0) << std::dec << std::setfill(' ') << '\n';
+}
+
 int main(int argc, char **argv)
 {
     if(argc < 2) {
@@ -78,17 +112,26 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    GPUCore core;
     std::vector<uint8_t> bytes = readFileContents(argv[1]);
+
+    for(auto b: { 0x73, 0x00, 0x10, 0x00 }) // EBREAK, in case code has no exit
+        bytes.push_back(b);
+
     bytes.resize(0x10000);
+    GPUCore core;
     Memory m(bytes);
-    core.x[1] = 0xffffffff;
-    do {
-        std::cout << "pc :";
-        std::cout << std::hex << std::setw(8) << std::setfill('0') << core.pc;
-        std::cout << " " << std::hex << std::setw(8) << std::setfill('0') << m.read32(core.pc);
-        std::cout << std::setw(0) << std::dec << std::setfill(' ') << '\n';
-        core.step(m);
-    } while(core.pc != 0xffffffff);
-    if(true) dumpGPUCore(core);
+
+    core.x[1] = 0xffffffff; // Set PC to unlikely value to catch ret with no caller
+
+    GPUCore::Status status;
+
+    try {
+        do {
+            disassemble(core.pc, m);
+            status = core.step(m);
+        } while(core.pc != 0xffffffff && status == GPUCore::RUNNING);
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        dumpGPUCore(core);
+    }
 }
