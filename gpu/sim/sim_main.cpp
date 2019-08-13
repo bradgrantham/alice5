663d18dac222471cba5c207e7efa0099d9f5a31b
Brad Grantham <grantham@plunk.org>
2019-08-12 17:17:48 -0700

    Read a shader binary
    Add a command-line parameter to sim_main that is a shader binary
      file, as written by as.cpp
    
    Fill block RAM with text bytes

--- gpu/sim/sim_main.cpp
+++ gpu/sim/sim_main.cpp
@@ -1,11 +1,65 @@
 #include <inttypes.h>
+#include <iomanip>
 #include <iostream>
+
+#include "objectfile.h"
 
 #include "VMain.h"
 
+void usage(const char* progname)
+{
+    printf("usage: %s shader.bin\n", progname);
+}
+
 int main(int argc, char **argv) {
+
+    bool beVerbose = false;
+
     Verilated::commandArgs(argc, argv);
     Verilated::debug(1);
+
+    char *progname = argv[0];
+    argv++; argc--;
+
+    while(argc > 0 && argv[0][0] == '-') {
+        if(strcmp(argv[0], "-v") == 0) {
+
+            beVerbose = true;
+            argv++; argc--;
+
+        } else if(strcmp(argv[0], "-h") == 0) {
+
+            usage(progname);
+            exit(EXIT_SUCCESS);
+
+        } else {
+
+            usage(progname);
+            exit(EXIT_FAILURE);
+
+        }
+    }
+
+    if(argc < 1) {
+        usage(progname);
+        exit(EXIT_FAILURE);
+    }
+
+    RunHeader2 header;
+    std::vector<uint8_t> text_bytes;
+    std::vector<uint8_t> data_bytes;
+    SymbolTable text_symbols;
+    SymbolTable data_symbols;
+
+    std::ifstream binaryFile(argv[0], std::ios::in | std::ios::binary);
+    if(!binaryFile.good()) {
+        throw std::runtime_error(std::string("couldn't open file ") + argv[0] + " for reading");
+    }
+
+    if(!ReadBinary(binaryFile, header, text_symbols, data_symbols, text_bytes, data_bytes)) {
+        exit(EXIT_FAILURE);
+    }
+    assert(text_bytes.size() % 4 == 0);
 
     VMain *top = new VMain;
 
@@ -17,6 +71,36 @@ int main(int argc, char **argv) {
         // Eval all Verilog code.
         top->eval();
     }
+
+    // Write text bytes to text memory
+    for(uint32_t address = 0; address < text_bytes.size(); address += 4) {
+        top->ext_address = address;
+        top->ext_write = 1;
+        uint32_t text_word = 
+            text_bytes[address + 0] << 24 |
+            text_bytes[address + 1] << 16 |
+            text_bytes[address + 2] <<  8 |
+            text_bytes[address + 3] <<  0;
+        top->ext_in_data = text_word;
+
+        if(beVerbose) {
+            std::cout << std::setfill('0');
+            std::cout << "Writing to " << address << " value 0x" << std::hex << std::setw(8) << top->ext_in_data << std::dec << std::setw(0) << "\n";
+        }
+
+        // Toggle clock.
+        top->clock = top->clock ^ 1;
+
+        // Eval all Verilog code.
+        top->eval();
+
+        // Toggle clock.
+        top->clock = top->clock ^ 1;
+
+        // Eval all Verilog code.
+        top->eval();
+    }
+
 
     uint32_t counter = 0;
 
