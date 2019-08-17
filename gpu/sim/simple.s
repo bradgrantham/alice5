a6d8834798fb32b1e379cb7c2b7c19790d1437bf
Brad Grantham <grantham@plunk.org>
2019-08-17 07:56:11 -0700

    use constants more recognizable in simulation

--- gpu/sim/simple.s
+++ gpu/sim/simple.s
@@ -1,6 +1,6 @@
 .segment text
-        addi x1, x0, 123
-        addi x2, x0, 321
+        addi x1, x0, 0x314
+        addi x2, x0, 0x111
         add x3, x1, x2
         lui x4, %hi(0x31415000)
         ebreak                        
