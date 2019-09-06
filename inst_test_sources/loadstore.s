ad2e04a449a22e3a3695f97f33bdf6fb2088a855
Brad Grantham <grantham@plunk.org>
2019-09-05 18:38:54 -0700

    add float load and store to test

--- inst_test_sources/loadstore.s
+++ inst_test_sources/loadstore.s
@@ -1,10 +1,19 @@
 .segment text
+        ; x regs
 	lui	a5,%hi(.pihex)
 	lw	a6,%lo(.pihex)(a5)
-	lui	a5,%hi(.result)
-	sw	a6,%lo(.result)(a5)
+	lui	a5,%hi(.result0)
+	sw	a6,%lo(.result0)(a5)
+
+        ; f regs
+	lui	a5,%hi(.one)
+	flw	fa0,%lo(.one)(a5)
+	lui	a5,%hi(.result1)
+	fsw	fa0,%lo(.result1)(a5)
         ebreak                        
 
 .segment data
 .pihex:         .word   0x31415927
-.result:        .word   0
+.result0:        .word   0
+.one:           .fword  1.0
+.result1:        .word   0
