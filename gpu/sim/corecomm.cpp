f3f5df02b40e301f001387cee68e8d72902d31a2
Brad Grantham <grantham@plunk.org>
2019-09-23 07:27:21 -0700

    update to match Verilog

--- gpu/sim/corecomm.cpp
+++ gpu/sim/corecomm.cpp
@@ -6,28 +6,33 @@
 
 // Proposed H2F interface for testing a single core
 const uint32_t h2f_reset_n_bit          = 0x80000000;
-const uint32_t h2f_cmd_bit              = h2f_reset_n_bit;
+const uint32_t h2f_not_reset            = h2f_reset_n_bit;
 const uint32_t h2f_run_bit              = 0x40000000;
-const uint32_t h2f_clk_bit              = 0x20000000;
-const uint32_t h2f_write_enable_bit     = 0x00800000;
-const uint32_t h2f_low_16_cmd_bit       = 0x00010000;
-const uint32_t h2f_high_16_cmd_bit      = 0x00020000;
-const uint32_t h2f_inst_ram_cmd_bit     = 0x00040000;
-const uint32_t h2f_data_ram_cmd_bit     = 0x00080000;
-const uint32_t h2f_xreg_cmd_bit         = 0x00080000;
-const uint32_t h2f_freg_cmd_bit         = 0x00100000;
-const uint32_t h2f_xreg_cmd_read_pc     = h2f_xreg_cmd_bit | 0x00000020;
+const uint32_t h2f_request_bit          = 0x20000000;
+const uint32_t h2f_cmd_put_low_16       = 0x00000000;
+const uint32_t h2f_cmd_put_high_16      = 0x00010000;
+const uint32_t h2f_cmd_write_inst_ram   = 0x00020000;
+const uint32_t h2f_cmd_write_data_ram   = 0x00030000;
+const uint32_t h2f_cmd_read_inst_ram    = 0x00040000;
+const uint32_t h2f_cmd_read_data_ram    = 0x00050000;
+const uint32_t h2f_cmd_read_x_reg       = 0x00060000;
+const uint32_t h2f_cmd_read_f_reg       = 0x00070000;
+const uint32_t h2f_cmd_read_special     = 0x00080000;
+const uint32_t h2f_special_PC           = 0x00080000;
+const uint32_t h2f_cmd_get_low_16       = 0x00090000;
+const uint32_t h2f_cmd_get_high_16      = 0x00100000;
 
-const uint32_t h2f_cmd_idle             = h2f_reset_n_bit;
-const uint32_t h2f_cmd_run              = h2f_reset_n_bit | h2f_run_bit;
+const uint32_t h2f_cmd_idle             = h2f_not_reset;
+const uint32_t h2f_cmd_request          = h2f_not_reset | h2f_request_bit;
+const uint32_t h2f_cmd_run              = h2f_not_reset | h2f_run_bit;
 
 // Proposed F2H interface for testing a single core
 const uint32_t f2h_run_halted_bit       = 0x80000000;
 const uint32_t f2h_run_exception_bit    = 0x40000000;
 const uint32_t f2h_run_exc_data_mask    = 0x00FFFFFF;
-const uint32_t f2h_cmd_phase_bit        = 0x00800000;
-const uint32_t f2h_phase_busy           = f2h_cmd_phase_bit;
-const uint32_t f2h_phase_ready          = f2h_cmd_phase_bit;
+const uint32_t f2h_busy_bit             = 0x20000000;
+const uint32_t f2h_phase_busy           = f2h_busy_bit;
+const uint32_t f2h_phase_ready          = 0;
 const uint32_t f2h_cmd_response_mask    = 0x0000FFFF;
 
 uint32_t *h2f;
@@ -43,9 +48,9 @@ const std::chrono::duration<float, std::micro> h2f_timeout_micros = 10us;
 bool waitOnPhaseOrTimeout(int phase)
 {
 
-    uint32_t phaseExpected = phase ? f2h_cmd_phase_bit : 0;
+    uint32_t phaseExpected = phase ? f2h_busy_bit : 0;
     auto start = std::chrono::high_resolution_clock::now();
-    while((*f2h & f2h_cmd_phase_bit) != phaseExpected) {
+    while((*f2h & f2h_busy_bit) != phaseExpected) {
 #if SIMULATE
         // cycle simulation clock
 #else
@@ -71,7 +76,7 @@ bool processCommand(uint32_t command)
     }
     
     // send the command and then wait for core to report it is processing the command
-    *h2f = command | h2f_clk_bit;
+    *h2f = command | h2f_not_reset | h2f_request_bit;
     if(!waitOnPhaseOrTimeout(f2h_phase_busy)) {
         std::cerr << "processCommand : timeout waiting on FPGA to indicate busy processing command\n";
         return false;
@@ -92,16 +97,16 @@ enum RamType { INST_RAM, DATA_RAM };
 void writeWord(uint32_t address, RamType ramType, uint32_t value)
 {
     // put low 16 bits into write register 
-    processCommand(h2f_cmd_bit | h2f_write_enable_bit | h2f_low_16_cmd_bit | (value & 0xFFFF));
+    processCommand(h2f_cmd_put_low_16 | (value & 0xFFFF));
 
     // put high 16 bits into write register 
-    processCommand(h2f_cmd_bit | h2f_write_enable_bit | h2f_high_16_cmd_bit | ((value >> 16) & 0xFFFF));
+    processCommand(h2f_cmd_put_high_16 | ((value >> 16) & 0xFFFF));
 
     // send the command to store the word from the write register into memory
     if(ramType == INST_RAM) {
-        processCommand(h2f_cmd_bit | h2f_write_enable_bit | h2f_inst_ram_cmd_bit | (address & 0xFFFF));
+        processCommand(h2f_cmd_write_inst_ram | (address & 0xFFFF));
     } else {
-        processCommand(h2f_cmd_bit | h2f_write_enable_bit | h2f_data_ram_cmd_bit | (address & 0xFFFF));
+        processCommand(h2f_cmd_write_data_ram | (address & 0xFFFF));
     }
 }
 
@@ -111,19 +116,19 @@ uint32_t readWord(uint32_t address, RamType ramType)
 
     // send the command to latch the word from memory into the read register
     if(ramType == INST_RAM) {
-        processCommand(h2f_cmd_bit | h2f_inst_ram_cmd_bit | (address & 0xFFFF));
+        processCommand(h2f_cmd_read_inst_ram | (address & 0xFFFF));
     } else {
-        processCommand(h2f_cmd_bit | h2f_data_ram_cmd_bit | (address & 0xFFFF));
+        processCommand(h2f_cmd_read_data_ram | (address & 0xFFFF));
     }
 
     // put low 16 bits of read register on low 16 bits of F2H
-    processCommand(h2f_cmd_bit | h2f_low_16_cmd_bit);
+    processCommand(h2f_cmd_get_low_16);
 
     // get low 16 bits into value
     value = *f2h & 0xFFFF;
 
     // put high 16 bits of read register on low 16 bits of F2H
-    processCommand(h2f_cmd_bit | h2f_high_16_cmd_bit);
+    processCommand(h2f_cmd_get_high_16);
 
     // get high 16 bits into value
     value = value | ((*f2h & 0xFFFF) << 16);
@@ -139,20 +144,23 @@ uint32_t readReg(RegisterKind kind, int reg)
 
     // send the command to latch the register into the read register
     if(kind == X_REG) {
-        processCommand(h2f_cmd_bit | h2f_xreg_cmd_bit | (reg & 0x1F));
+        processCommand(h2f_cmd_read_x_reg | (reg & 0x1F));
     } else if(kind == PC_REG) {
-        processCommand(h2f_cmd_bit | h2f_xreg_cmd_read_pc);
+        processCommand(h2f_cmd_read_special | h2f_special_PC);
     } else {
-        processCommand(h2f_cmd_bit | h2f_freg_cmd_bit | (reg & 0x1F));
+        processCommand(h2f_cmd_read_f_reg | (reg & 0x1F));
     }
 
     // put low 16 bits of read register on low 16 bits of F2H
-    processCommand(h2f_cmd_bit | h2f_low_16_cmd_bit);
+    processCommand(h2f_cmd_get_low_16);
 
+    // get low 16 bits into value
     value = *f2h & f2h_cmd_response_mask;
 
     // put high 16 bits of read register on low 16 bits of F2H
-    processCommand(h2f_cmd_bit | h2f_high_16_cmd_bit);
+    processCommand(h2f_cmd_get_high_16);
+
+    // get high 16 bits into value
     value = value | ((*f2h & f2h_cmd_response_mask) << 16);
 
     return value;
